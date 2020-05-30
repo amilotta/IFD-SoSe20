191077649126144d3631035a8b8daadcc655d692
Amelie Bachmann <64253310+amilotta@users.noreply.github.com>
2020-05-30 15:46:41 +0200

    VUI

--- VUI/js/playgroud-artyom-script.js
+++ VUI/js/playgroud-artyom-script.js
@@ -21,9 +21,37 @@ window.addEventListener("load", function () {
         indexes: ["Wie lautet dein Name"],
         smart: false,
         action: function (i, wildcard) {
-            artyom.say("Mein Name is Artyom");
+            artyom.say("Mein Name ist Laura");
         }
     });
+
+    artyom.addCommands({
+        indexes: ["Wer hat heute Geburtstag?"],
+        smart: false,
+        action: function (i, wildcard) {
+            artyom.say("Heute ist Lucia Bachmann's Geburtstag. Sie wird heute 21 Jahre alt.");
+        }
+    });
+  
+    artyom.addCommands({
+        indexes: ["Was gibt es Neues?"],
+        smart: false,
+        action: function (i, wildcard) {
+            artyom.say("Um 15 Uhr kommt der Diakoniedienst. Soll ich etwas ausrichten?");
+        }
+    });
+
+    artyom.addCommands({
+        indexes: ["Richte dem Diakoniediesnt folgendes aus: Meine Milch ist ausgelaufen, ich brauche eine Neue."],
+        smart: false,
+        action: function (i, wildcard) {
+            artyom.say("Ich leite dem Diakoniediesnt weiter: Meine Milch ist ausgelaufen, ich brauche eine Neue.");
+        }
+    });
+
+
+  
+
 
     function startContinuousArtyom() {
         artyom.fatality();
